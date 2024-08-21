59a3bd0cfaf91b1d731ebfc5fb986fc156f7d5ee
AshuJojo <ashutoshsharma.120599@gmail.com>
2024-08-21 13:33:52 +0530

    Navbar & Hero Section Completed

---
+++ qtify/src/components/Home.jsx
@@ -0,0 +1,11 @@
+import Hero from "./Hero/Hero";
+import Navbar from "./Navbar/Navbar"
+
+const Home = () => {
+    return <>
+        <Navbar />
+        <Hero />
+    </>
+}
+
+export default Home;
